1eed2c431add8a7031251c277a082d1d1daf7af7
Your New Name <vann7568@gmail.com>
2026-01-27 13:06:00 -0700

    added Person required classes

--- Person.java
+++ Person.java
@@ -1,3 +1,18 @@
 public abstract class Person {
-    
+    String myName;
+    String occupation;
+
+    public void askName(){
+        System.out.println("What is your name?");
+    }
+    public void giveName(){
+        System.out.println("My name is "+myName);
+    }
+
+    public void whatIDo(){
+        System.out.println("I am a "+occupation);
+    }
+
+    public abstract void askQuestion();
+    public abstract void answerQuestion();
 }
